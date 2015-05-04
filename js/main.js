abfc3cd3625a7004cc4757eede3a57b400e92738
Katherine Trammell <ktrammell@gmail.com>
2015-05-03 23:26:56 -0400

    made adjustments to header and tiy section for 788px width

--- js/main.js
+++ js/main.js
@@ -9,55 +9,44 @@
  */
 
 ( function( $ ) {
-  
-  // Setup variables
-  $window = $(window);
-  $slide = $('.homeSlide');
-  $body = $('body');
- 
-  $window.ready(function(){
-    $('html,body').animate({ scrollTop: 7000 }, 0);   
-});
-
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
     //FadeIn all sections   
-  $body.imagesLoaded( function() {
-    setTimeout(function() {
-          
-          // Resize sections
-          adjustWindow();
-          
-          // Fade in sections
-        $body.removeClass('loading').addClass('loaded');
-        
-    }, 800);
-  });
-  
-  function adjustWindow(){
-    
-    // Init Skrollr
-    var s = skrollr.init({
-          render: function(data) {
-              //Log the current scroll position.
-              console.log(data.curTop);
-          },
-            forceHeight: false
-        });
-    
-    // Get window size
-      winH = $window.height();
-      
-      // Keep minimum height 550
-      if(winH <= 550) {
-      winH = 550;
-    } 
-      
-      // Resize our slides
-      $slide.height(winH);
-      
-      // Refresh Skrollr after resizing our sections 
-      
-  }
-
-
-    
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    
+	    
+	}
+		
 } )( jQuery );
